082b2bc199d29b911fb512da2765a6b867875281
Mohammed-Rabeeh-KT <rabeehkt77@gmail.com>
2026-02-07 00:36:32 +0530

    implemented sales page and also coupon management

--- middlewares/adminAuth.js
+++
@@ -1,9 +0,0 @@
-import AppError from "../utils/AppError.js";
-import { STATUS } from "../utils/response.js";
-
-export const protectAdmin = (req, res, next) => {
-    if(!req.session.admin){
-        return next(new AppError("Please log in as admin", STATUS.UNAUTHORIZED));
-    }
-    next();
-}
--- middlewares/cacheControlMiddleware.js
+++
@@ -1,8 +0,0 @@
-const cacheControlMiddleware = (req, res, next) => {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate, max-age=0');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    next();
-};
-
-export default cacheControlMiddleware;
