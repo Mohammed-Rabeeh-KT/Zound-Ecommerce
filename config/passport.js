1af9e55d38da43d55153beca9a39297685bc289a
Mohammed-Rabeeh-KT <rabeehkt77@gmail.com>
2026-03-25 17:29:43 +0530

    fix : google auth bugs

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || 'https://zound.rabeehkt.in/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
